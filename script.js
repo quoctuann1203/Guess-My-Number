af06ac9d0f3f8fb2eb00f15794a191d4e6564bb0
Quoc Tuan <you@example.com>
2022-01-19 11:50:28 +0700

    finish button again

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ let d = document.querySelector('.guess').value = 12;
 console.log(d); */
 
 //////////////////////////
-const sercertNumber = Math.trunc(Math.random() * 20) + 1;
+let sercertNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,9 +27,14 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === sercertNumber) {
     document.querySelector('.message').textContent = '🎆 Correct Number';
     document.querySelector('.number').textContent = sercertNumber;
-    document.querySelector('.highscore').textContent = score;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When number in input > sercert Number
   } else if (guess > sercertNumber) {
@@ -52,4 +58,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+//Buttion Again
+document.querySelector('.again').addEventListener('click', function () {
+  // location.reload();
+  sercertNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
